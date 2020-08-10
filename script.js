8a79b9aa00051a565961dd2b862e374057d081e4
Y. Violet Guo <yueguo598@gmail.com>
2020-08-10 13:40:35 -0300

    hardcode select background change

--- script.js
+++ script.js
@@ -3,9 +3,8 @@
 // You can access the RESULTS_MAP from "constants.js" in this file since
 // "constants.js" has been included before "script.js" in index.html.
 
-const ans_w = document.querySelectorAll('[data-question-id="one"]');
-const x = ans_w[0].querySelectorAll(".checkbox");
+const ans_1 = document.querySelectorAll('[data-question-id="one"]');
 
-x[0].addEventListener('click', function(){
-    x[0].style.backgroundColor = 'red';
+ans_1[0].addEventListener('click', function(){
+    ans_1[0].style.backgroundColor = 'red';
 });
