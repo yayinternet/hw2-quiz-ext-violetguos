cc2443ba63f164036ae2070c626f2992cb6ad58b
Y. Violet Guo <yueguo598@gmail.com>
2020-08-10 13:01:26 -0300

    test js script background color change

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 
 // You can access the RESULTS_MAP from "constants.js" in this file since
 // "constants.js" has been included before "script.js" in index.html.
+
+const ans_w = document.querySelectorAll('[data-question-id="one"]');
+const x = ans_w[0].querySelectorAll(".checkbox");
+
+x[0].addEventListener('click', function(){
+    x[0].style.backgroundColor = 'red';
+});
